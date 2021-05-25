ff4ada259d776ad3d20f20dd0c611e9a82d4091c
Tazribine Hassan <tazribinehassan1998@gmail.com>
2021-05-25 00:17:34 +0000

    added the login pages

---
+++ GestionDesAbsence/Controllers/LoginController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionDesAbsence.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+                
+        
+        public ActionResult IndexMsg(string msg)
+        {
+            ViewBag.Msg = msg;
+            return View("Index");
+        }
+
+        [HttpPost]
+        public string CheckAdmin(string email, string password)
+        {
+            if (email == "admin@gmail.com" && password == "123")
+            {
+                return "you're in";
+            }
+            else
+            {
+                return "please check your email";
+            }
+        }
+
+        [HttpPost]
+        public string CheckTeacher(string email, string password)
+        {
+            if (email == "teacher@gmail.com" && password == "123")
+            {
+                return "you're in";
+            }
+            else
+            {
+                return "please check your email";
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CheckStudent(string email, string password)
+        {
+            if(email == "student@gmail.com" && password == "123")
+            {
+                
+                return View("Home");
+            }
+            else
+            {
+                return RedirectToAction("IndexMsg", new { msg = "Email or password are incorrect" });
+            }
+        }        
+        
+    }
+}
