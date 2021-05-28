7230afc6e8a5cbfd98b1069c11ca4fc81f06f8d5
saadrds <saadrds@gmail.com>
2021-05-28 21:06:43 +0100

    added newBranche saad_Safae

---
+++ GestionDesAbsence/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionDesAbsence.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult AllFilieres()
+        {
+            return View();
+        }
+
+        public ActionResult AjouterClasse()
+        {
+            return View();
+        }
+    }
+}
