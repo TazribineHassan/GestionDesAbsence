581cb638b513ca1e7a22659ffcc2ea64b7bb4e57
safaeelhimer <safae.elhimer24@gmail.com>
2021-06-01 14:01:21 +0100

    adding profs and students with excel done

--- GestionDesAbsence/Controllers/AdminController.cs
+++ GestionDesAbsence/Controllers/AdminController.cs
@@ -5,17 +5,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Data.OleDb;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Web.Mvc;
+using LinqToExcel;
+using System.Data.Entity.Validation;
+using System.IO;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
 
 namespace GestionDesAbsence.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
-        public ActionResult Index()
+        /*public ActionResult Index()
         {
             return View();
-        }
+        }*/
 
         public ActionResult Home()
         {
@@ -32,13 +41,22 @@ namespace GestionDesAbsence.Controllers
             return View();
         }
 
-        public ActionResult ExcelPage()
+        public ActionResult AjouterProfesseur()
+        {
+            return View();
+        }
+        public ActionResult AjouterEtudiants()
         {
             GestionDesAbsenceContext gestion = new GestionDesAbsenceContext();
             ViewBag.list = new SelectList(gestion.Cycles, "Id", "Nom");
-
             return View();
         }
+
+        public ActionResult ExcelPage()
+        {
+            return View();
+        }
+
         public JsonResult GetClass(int id)
         {
             GestionDesAbsenceContext gestion = new GestionDesAbsenceContext();
@@ -58,7 +76,7 @@ namespace GestionDesAbsence.Controllers
         }
 
         [HttpPost]
-        public ActionResult SaveEtudiant(string cne, string nom, String prenom,string email,string cycle, string classe) 
+        public ActionResult SaveEtudiant(string cne, string nom, String prenom, string email, string cycle, string classe)
         {
             GestionDesAbsenceContext gestion = new GestionDesAbsenceContext();
             Etudiant e = new Etudiant();
@@ -71,7 +89,7 @@ namespace GestionDesAbsence.Controllers
             AdminService service = new AdminService();
             service.saveEtudiant(e);
             ViewBag.list = new SelectList(gestion.Cycles, "Id", "Nom");
-             return Redirect("/Admin/AllEtudiant");
+            return Redirect("/Admin/AllEtudiant");
         }
 
         public ActionResult DeleteEtudiant(String id)
@@ -82,5 +100,96 @@ namespace GestionDesAbsence.Controllers
             service.deleteEtudiant(e);
             return Redirect("/Admin/AllEtudiant");
         }
+
+        GestionDesAbsenceContext db = new GestionDesAbsenceContext();
+
+
+        [ActionName("Index")]
+        [HttpPost]
+        public ActionResult import( int myclass)
+        {
+            HttpPostedFileBase file = Request.Files["file"];
+            if (file == null || file.ContentLength <= 0)
+            {
+                return Json("please select excel file", JsonRequestBehavior.AllowGet);
+            }
+            Stream streamfile = file.InputStream;
+            DataTable dt = new DataTable();
+            string FileName = Path.GetExtension(file.FileName);
+            if (FileName != ".xls" && FileName != ".xlsx")
+            {
+                return Json("Only excel file", JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                try
+                {
+                    if (FileName == ".xls")
+                    {
+                        HSSFWorkbook workbook = new HSSFWorkbook(streamfile);
+                        dt = excel.Import(dt, workbook, db, myclass);
+                    }
+                    else
+                    {
+                        XSSFWorkbook workbook = new XSSFWorkbook(streamfile);
+                        dt = excel.Import(dt, workbook, db, myclass);
+                    }
+                    return Json("OK", JsonRequestBehavior.AllowGet);
+                }
+
+                catch (Exception e)
+                {
+                    
+                    return Json( e.ToString(), JsonRequestBehavior.AllowGet);
+                }
+            }
+            // return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult AddProfs()
+        {
+            HttpPostedFileBase file = Request.Files["file"];
+            if (file == null || file.ContentLength <= 0)
+            {
+                return Json("please select excel file", JsonRequestBehavior.AllowGet);
+            }
+            Stream streamfile = file.InputStream;
+            DataTable dt = new DataTable();
+            string FileName = Path.GetExtension(file.FileName);
+            if (FileName != ".xls" && FileName != ".xlsx")
+            {
+                return Json("Only excel file", JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                try
+                {
+                    if (FileName == ".xls")
+                    {
+                        HSSFWorkbook workbook = new HSSFWorkbook(streamfile);
+                        dt = ExcelP.Importing(dt, workbook, db);
+                    }
+                    else
+                    {
+                        XSSFWorkbook workbook = new XSSFWorkbook(streamfile);
+                        dt = ExcelP.Importing(dt, workbook, db);
+                    }
+                    return Json("OK", JsonRequestBehavior.AllowGet);
+                }
+
+                catch (Exception e)
+                {
+
+                    return Json(e.ToString(), JsonRequestBehavior.AllowGet);
+                }
+            }
+            // return View();
+        }
+
+
+
+
     }
 }
